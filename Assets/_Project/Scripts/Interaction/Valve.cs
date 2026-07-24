9ff1395075883c948ff859ec713c3721850d928f
Jbucklin2 <Jmbucklin2200@gmail.com>
2026-07-24 01:35:45 -0400

    Added Audio Manager and basic sound
    Added audio to the valve test. Audio now works as intended.

--- Assets/_Project/Scripts/Interaction/Valve.cs
+++ Assets/_Project/Scripts/Interaction/Valve.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using FMODUnity;
 
 public class Valve : MonoBehaviour, IInteractable
 {
     [SerializeField] private UnityEvent onActivated;
+    [SerializeField] private FMODUnity.EventReference openSound;
 
     private bool isActivated;
 
@@ -13,6 +15,7 @@ public class Valve : MonoBehaviour, IInteractable
         if (isActivated)
             return;
 
+        AudioManager.instance.PlayOneShot(openSound, this.transform.position);
         isActivated = true;
 
         Debug.Log(gameObject.name + " activated!");
